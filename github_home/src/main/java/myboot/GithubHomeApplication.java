376db84839cc03538535d9e8cb1a0be19c6550ec
me <bluejeansh@hanmail.net>
2022-03-20 11:26:48 +0900

    other second commit

--- github_home/src/main/java/myboot/GithubHomeApplication.java
+++ github_home/src/main/java/myboot/GithubHomeApplication.java
@@ -9,6 +9,7 @@ public class GithubHomeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GithubHomeApplication.class, args);
 		System.out.println("깃헙부트실행");
+		System.out.println("수정후 깃헙 푸쉬");
 	}
 
 }
